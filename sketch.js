0bbeef8fc50a99c778efb38bccea9f62c7ce6264
Ana Zeneli <az2498@columbia.edu>
2019-01-23 14:53:03 -0500

    Updated colors to look more like a sunset

--- sketch.js
+++ sketch.js
@@ -3,18 +3,18 @@ function setup() {
 }
 
 function draw() {
-    let b = color(212,175,55);
+    let b = color(100,20,100);
     background(b);
     translate(width/2, 3*height/4);
     rotate(-PI);
-    let h = color(200, 0, 0);
+    let h = color(232, 120, 80);
     fill(h);
     // fill 24-hour semi-cicle
     arc(0, 0, 600, 600, 0, PI*(hour()/24));
     noFill()
     arc(0, 0, 600, 600, 0, PI);
     
-    let m = color(255, 70, 0);
+    let m = color(253, 94, 83);
     // fill minute semi-cicle
     fill(m);
     arc(0, 0, 400, 400, 0, PI*(minute()/59))
